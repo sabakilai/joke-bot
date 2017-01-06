68ab4a99b42f2d6ce54c01bc66de8a4d07fa6d12
elaman <elaman.bazarov98@gmail.com>
2017-01-06 13:32:39 +0600

    done

--- routes/index.js
+++ routes/index.js
@@ -57,12 +57,14 @@ router.post("/", function(req, res, next) {
     else if(content == "/off") {
       db.update({state: false},{where: {userId: userId,ip: ip}});
       let message = "Вы отключили ежедневную рассылку.Чтобы включить обратно,введите команду '/on'";
+      console.log(message);
       sms(message, chatId, TOKEN, ip);
 
     }
     else if(content == "/on") {
       db.update({state: true},{where: {userId: userId,ip: ip}});
       let message = "Вы включили ежедневную рассылку.Чтобы выключить обратно,введите команду '/off'";
+      console.log(message);
       sms(message, chatId, TOKEN, ip);
       
     }
