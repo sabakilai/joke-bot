909613be29572e8604b5293b132775f56211def1
elaman <elaman.bazarov98@gmail.com>
2017-01-17 18:13:06 +0600

    done

--- routes/index.js
+++ routes/index.js
@@ -11,77 +11,78 @@ router.get('/', function(req, res, next) {
 });
 
 router.post("/", function(req, res, next) {
-  var event = req.body.event;
   var ip = req.connection.remoteAddress;
-
-  var errMessage = "Некорректный ввод.Введите нужную цифру:\n\uE21Cполучить случайный анекдот.\n\uE21Dполучить 20 случайных анекдотов.\n\uE21Eотключить ежедневную рассылку,введите команду '/off'";
-  if(event == "user/unfollow") {
-  	let userId = req.body.data.id;
-  	db.destroy({where:{userId: userId, ip: ip}});
-  	console.log("db destroyed");
-  }
-  if(event == "user/follow") {
-  let userId = req.body.data.id;
-  db.create({userId: userId, ip: ip});
-  console.log("user follows");
-  newChat(userId, ip, function(err, res, body) {
-  	let chatId = body.data.id;
-  	let message = "Здравствуйте!Я буду присылать вам самые свежие анекдоты. Введите нужную цифру:\n\uE21Cполучить случайный анекдот.\n\uE21Dполучить 20 случайных анекдотов.\n\uE21Eотключить ежедневную рассылку,введите команду '/off'";
-    sms(message, chatId, ip);
-  })
-  }
-  if(event == "message/new") {
-    var userId = req.body.data.sender_id;
-  	var content = req.body.data.content;
-  	var chatId = req.body.data.chat_id;
-  	if(req.body.data.type != 'text/plain') {
-  		console.log(errMessage);
-  		sms(errMessage, chatId, ip);
-  		return;
-  	}
-    
-    if(content == "1") {
-     parse.getRandomJoke(function(result) {
-     	console.log(result);
-     	sms(result, chatId, ip);
-     })
+  db.find({
+    where: {
+      userId: req.body.data.id || req.body.data.sender_id, 
+      ip: ip 
     }
-    else if(content == "2") {
-        	parse.getJokes(function(result) {
-          	for(var idx in result) {
-    			console.log(result[idx]);
-    			sms(result[idx], chatId, ip);
-   		}
-     })
+  }).then(function(user) {
+    var event = req.body.event;
+    var commandMessage = function(user) {
+      return "Введите нужную цифру:\n\uE21Cполучить случайный анекдот.\n\uE21Dполучить 20 случайных анекдотов.\n\uE21E"+(user.state ? "Отключить" : "Включить")+" ежедневную рассылку.";
     }
-    else if(content == "3") {
-      db.find({where: {state: false, userId: userId, ip: ip}})
-      .then(function(user) {
-        console.log(user);
-        let message = "";
-        if(user) {
-          message = "Рассылка уже отключена.\n\uE21FВключить обратно";
-        } else {
-          db.update({state: false},{where: {userId: userId,ip: ip}});
-          message = "Вы отключили ежедневную рассылку.\n\uE21FВключить обратно";
-        }
-        console.log(message);
-        sms(message, chatId, ip);
-      })
-
+    var errMessage = "Некорректный ввод." + commandMessage(user);
+    if(event == "user/unfollow") {
+    	let userId = req.body.data.id;
+    	db.destroy({where:{userId: userId, ip: ip}});
+    	console.log("db destroyed");
     }
-    else if(content == "4") {
-      db.update({state: true},{where: {userId: userId,ip: ip}});
-      let message = "Вы включили ежедневную рассылку.\n\uE21CВыключить обратно";
-      console.log(message);
+    if(event == "user/follow") {
+    let userId = req.body.data.id;
+    db.create({userId: userId, ip: ip});
+    console.log("user follows");
+    newChat(userId, ip, function(err, res, body) {
+    	let chatId = body.data.id;
+    	let message = "Здравствуйте!Я буду присылать вам самые свежие анекдоты." + commandMessage(user);
       sms(message, chatId, ip);
+    })
+    }
+    if(event == "message/new") {
+      var userId = req.body.data.sender_id;
+    	var content = req.body.data.content;
+    	var chatId = req.body.data.chat_id;
+    	if(req.body.data.type != 'text/plain') {
+    		console.log(errMessage);
+    		sms(errMessage, chatId, ip);
+    		return;
+    	}
       
+      if(content == "1") {
+       parse.getRandomJoke(function(result) {
+       	console.log(result);
+       	sms(result, chatId, ip);
+       })
+      }
+      else if(content == "2") {
+          	parse.getJokes(function(result) {
+            	for(var idx in result) {
+      			console.log(result[idx]);
+      			sms(result[idx], chatId, ip);
+     		}
+       })
+      }
+      else if(content == "3") {
+        db.find({where: {userId: userId, ip: ip}})
+        .then(function(user) {
+          if(user.state) {
+            db.update({state: false}, {where: {userId: userId, ip: ip}}).then(function(user) {
+              let message = "Вы отключили ежедневную рассылку."+commandMessage(user);
+              sms(message, chatId, ip);    
+            })
+          } else {
+            db.update({state: true}, {where: {userId: userId, ip: ip}}).then(function(user) {
+              let message = "Вы включили ежедневную рассылку."+commandMessage(user);
+            })
+          }
+        })
+      };
+      else {
+        console.log(errMessage);
+    		sms(errMessage, chatId, ip);
+      }
     }
-    else {
-   console.log(errMessage);
-  		sms(errMessage, chatId, ip);
-    }
-  }
-  res.end();
+    res.end();
+  })
 })
 module.exports = router;
