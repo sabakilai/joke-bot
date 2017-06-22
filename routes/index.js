988b055c4fd9b16650d4e39f398358422441027d
bahautdinov_bahtiyar <sabakilai@gmail.com>
2017-06-22 19:37:18 +0600

    First commit

--- routes/index.js
+++ routes/index.js
@@ -56,13 +56,12 @@ router.post("/", function(req, res, next) {
 
         if(content == "1") {
           //set region to proper
-            console.log(chui.second_table.row_1.name);
             var svodka = function () {
-              return chui.text.first_day + chui.text.second_day ;//+ "\n" +
-                     //chui.second_table.row1.name +
-                     //" днем: " + chui.second_table.row1.day_temp + " ночью: " + chui.second_table.row1.day_temp + "\n" +
-                     //chui.second_table.row2.name +
-                     //" днем: " + chui.second_table.row2.day_temp + " ночью: " + chui.second_table.row2.day_temp
+              return chui.text.first_day + chui.text.second_day + "\n" +
+                     chui.second_table.row1.name +
+                     " днем: " + chui.second_table.row1.day_temp + " ночью: " + chui.second_table.row1.day_temp + "\n" +
+                     chui.second_table.row2.name +
+                     " днем: " + chui.second_table.row2.day_temp + " ночью: " + chui.second_table.row2.day_temp
             }
             var message = "Вы установили рассылку на Чуйскую и Таласcкую области. Вот последняя сводка по этому региону";
 
