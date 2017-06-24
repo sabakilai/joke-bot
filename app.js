bc1523427ddb76a32910880a40683b60215baf84
bahautdinov_bahtiyar <sabakilai@gmail.com>
2017-06-24 21:54:06 +0600

    First commit

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ var sms = require("./models/sms.js");
 var newChat = require("./models/newchat.js");
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var CronJob = require('cron').CronJob;
+var job = require('./libs/job')
 var TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NDM0LCJwaG9uZSI6IjEyMzQ1Njc4Njc1NDUzNDI0MyIsInBhc3N3b3JkIjoiJDJhJDEwJHV4TnlHSlBuYkJjMUNDSVFLUi9nNWV4UnFNN1c0QUQyR2hIQzRBdWJwN1V5dVlnbDBRRWRPIiwiaXNCb3QiOnRydWUsImNvdW50cnkiOnRydWUsImlhdCI6MTQ4MzU5NTI5N30.ZLZ9BtUwhJqSMJgWc0Ln7iYT7W944BB4RAIIDMVkzg8";
 var app = express();
 
