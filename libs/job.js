a4b17c5c59c5394d5ddd3325ff66925b613bac5b
bahautdinov_bahtiyar <sabakilai@gmail.com>
2017-07-01 19:15:52 +0600

    Init

--- libs/job.js
+++ libs/job.js
@@ -17,15 +17,15 @@ module.exports = {
   MainJob(){
     let message = [];
     message.push(Chui());
-    //message.push(Osh());
-    //message.push(Naryn());
-  //  message.push(Isyk());
-    //message.push(Capitals());
-    //message.push(GetMesMessage());
+    message.push(Osh());
+    message.push(Naryn());
+    message.push(Isyk());
+    message.push(Capitals());
+    message.push(GetMesMessage());
 
     Promise.all(message).then((messages)=>{
       console.log(messages);
-      //SendDailyMessages();
+      SendDailyMessages();
     }).catch(
         (err)=>{console.log(err);}
       )
